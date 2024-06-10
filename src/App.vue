62ee050411194793af879b104a7a663cebe6badf
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-06-09 23:41:43 -0500

    Web structured.

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,27 @@
     <div class="container">
         <h1>QR Generator</h1>
         <div class="form">
-            <input type="text" placeholder="URL or Text to generate" />
-            <button>Generate</button>
+            <input type="text" v-model="value2generate" placeholder="URL or Text to generate" />
+            <button @click="generate">Generate</button>
+            <p v-if="errorMessage" class="alert alert-danger" role="alert">{{ errorMessage }}</p>
         </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const errorMessage = ref('');
+const value2generate = ref('');
+
+const generate = () => {
+    if (value2generate.value.trim() === '') {
+        errorMessage.value = `El campo de texto no puede estar vacío.`;
+    } else {
+        // generar QR
+    }
+};
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
@@ -23,6 +37,23 @@ html {
 *::before,
 *::after {
     box-sizing: border-box;
+}
+input {
+    font-size: 1.1em;
+    padding: 0.5em 1em;
+}
+.alert {
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+    margin-bottom: 1rem;
+    padding: 0.75rem 1.25rem;
+    position: relative;
+
+    &.alert-danger {
+        background-color: #f8d7da;
+        border-color: #f5c6cb;
+        color: #721c24;
+    }
 }
 .container {
     margin-left: auto;
@@ -43,5 +74,11 @@ html {
     @media (min-width: 1200px) {
         max-width: 1140px;
     }
+}
+.form {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
 }
 </style>
